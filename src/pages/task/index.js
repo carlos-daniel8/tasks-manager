f08acdf35028bd6a6dbf33808197465bb6b52462
Carlos Daniel <carlosdevops22@gmail.com>
2024-04-08 02:21:54 -0300

    mock list

--- src/pages/task/index.js
+++ src/pages/task/index.js
@@ -5,7 +5,7 @@ export default function Task(){
     return (
         <div className="task-container">
             <header>
-                    <h1>Gerenciador de Tarefas</h1>
+                    <h1 className="title">Gerenciador de Tarefas</h1>
                     <button className="task-button">
                         <Link to="task/new">Adicionar nova tarefa</Link>
                     </button>
@@ -13,6 +13,69 @@ export default function Task(){
                         <h3>Sair</h3>
                     </button>
             </header>
+            <h1 className="list-title">Lista de Tarefas</h1>
+            <ul>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Mudar a cor do botão X</p>
+                    <strong>Descrição</strong>
+                    <p>Alteração feita a pedido do designer da sprint</p>
+                    <strong>Categoria</strong>
+                    <p>Sustentação</p>
+                    <strong>Status</strong>
+                    <p>Em andamento</p>
+                    <strong>Data de Criação</strong>
+                    <p>08/04/2024</p>
+
+                    <button className="update-button">Editar</button>
+                    <button className="delete-button">Excluir</button>
+                </li>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Mudar a cor do botão X</p>
+                    <strong>Descrição</strong>
+                    <p>Alteração feita a pedido do designer da sprint</p>
+                    <strong>Categoria</strong>
+                    <p>Sustentação</p>
+                    <strong>Status</strong>
+                    <p>Em andamento</p>
+                    <strong>Data de Criação</strong>
+                    <p>08/04/2024</p>
+                    
+                    <button className="update-button">Editar</button>
+                    <button className="delete-button">Excluir</button>
+                </li>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Mudar a cor do botão X</p>
+                    <strong>Descrição</strong>
+                    <p>Alteração feita a pedido do designer da sprint</p>
+                    <strong>Categoria</strong>
+                    <p>Sustentação</p>
+                    <strong>Status</strong>
+                    <p>Em andamento</p>
+                    <strong>Data de Criação</strong>
+                    <p>08/04/2024</p>
+                    
+                    <button className="update-button">Editar</button>
+                    <button className="delete-button">Excluir</button>
+                </li>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Mudar a cor do botão X</p>
+                    <strong>Descrição</strong>
+                    <p>Alteração feita a pedido do designer da sprint</p>
+                    <strong>Categoria</strong>
+                    <p>Sustentação</p>
+                    <strong>Status</strong>
+                    <p>Em andamento</p>
+                    <strong>Data de Criação</strong>
+                    <p>08/04/2024</p>
+                    
+                    <button className="update-button">Editar</button>
+                    <button className="delete-button">Excluir</button>
+                </li>
+            </ul>
         </div>
     )
 }
