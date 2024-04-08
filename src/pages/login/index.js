7e02da7dec883b8b7ce2d7212f0331d282000fdb
Carlos Daniel <carlosdevops22@gmail.com>
2024-04-07 22:48:48 -0300

    updates login interface

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,14 +1,13 @@
 import './styles.css';
-import padlock from '../../assets/padlock.png'
 
 export default function Login(){
     return (
         <div className="login-container">
             <section className="form">
                 <form>
-                    <h1 className='login-text'>Login</h1>
-                    <input placeholder='Username'/>
-                    <input placeholder='Password'/>
+                    <h1>Login</h1>
+                    <input placeholder="Username"/>
+                    <input placeholder="Password"/>
                     <button type="submit" className="button">Entrar</button>
                 </form>
             </section>
